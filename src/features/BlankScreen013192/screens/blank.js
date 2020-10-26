105958f6569c82ef929222096c7d5d3e991c913d
AlineTavares <aline@crowdbotics.com>
2020-10-26 14:19:14 -0400

    Update screen

--- src/features/BlankScreen013192/screens/blank.js
+++ src/features/BlankScreen013192/screens/blank.js
@@ -27,7 +27,7 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View style={styles.container}>
-      <Text>This is your new component</Text>
+      <Text>Hey I am updating my module here.</Text>
     </View>
   );
 }
